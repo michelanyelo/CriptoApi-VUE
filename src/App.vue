ae9f7f4ed788e4c3e22d94971a8ce8ad1d6d2063
MUNI <miguelmonzon.inf@gmail.com>
2024-10-30 12:02:24 -0300

    "Removed HelloWorld component and style.css, updated App.vue to include a form for selecting a cryptocurrency, and added styles for the new layout."

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const monedas = ref([
+    { codigo: 'USD', texto: 'Dolar de Estados Unidos' },
+    { codigo: 'CLP', texto: 'Peso Chileno' },
+    { codigo: 'EUR', texto: 'Euro' },
+    { codigo: 'GBP', texto: 'Libra Esterlina' },
+])
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <div class="contenedor">
+        <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+        <div class="contenido">
+            <form class="formulario">
+                <div class="campo">
+                    <label for="moneda">Moneda:</label>
+                    <select id="moneda">
+                        <option value="">-- Selecione --</option>
+                        <option v-for="moneda, idx in monedas" :key="idx" :value="moneda.codigo">{{ moneda.texto }}
+                        </option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
